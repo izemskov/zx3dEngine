6cd1d7c50da5113407a5a14af7c02e8dcb0c0968
izemskov <pascal.ilya@gmail.com>
2021-06-17 16:03:49 +0300

    Malloc test

--- src/main.c
+++ src/main.c
@@ -4,13 +4,34 @@
  *
  * Copyright 2021 Ilya Zemskov */
 
+/*
+	Compile program cmd:
+		zcc +zx -v -create-app -o zx3dEngine main.c engine.c point.c
+*/
+
+#include <malloc.h>
+#include <stdio.h>
+
 #include "engine.h"
 
-int main()
-{
+long heap;
+
+int main() {
+	mallinit();
+	sbrk(30000, 2000);
+
+	int * a = (int *) malloc(20 * sizeof(int));
+
+	a[0] = 20;
+	a[1] = 20;
+	a[2] = 30;
+	a[3] = 40;
+
 	initContext();
 
-	drawLine(10, 10, 50, 50);
+	drawLine(a[0], a[1], a[2], a[3]);
+
+	free(a);		
 
 	return 0;
 }
